e47a270f85d3c5060e05251eecb8ff6e0adf3a09
Maik <maki99999@web.de>
2024-12-20 18:27:30 +0100

    1.20.3 compatible version

--- common/src/main/java/io/github/maki99999/biomebeats/gui/SimpleTooltipHolder.java
+++ common/src/main/java/io/github/maki99999/biomebeats/gui/SimpleTooltipHolder.java
@@ -2,14 +2,10 @@ package io.github.maki99999.biomebeats.gui;
 
 import io.github.maki99999.biomebeats.util.Rect;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.Tooltip;
-import net.minecraft.client.gui.narration.NarrationElementOutput;
+import net.minecraft.client.gui.navigation.ScreenRectangle;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.tooltip.MenuTooltipPositioner;
-import net.minecraft.network.chat.Component;
-import org.jetbrains.annotations.NotNull;
 
 public class SimpleTooltipHolder {
     private Tooltip tooltip;
@@ -32,19 +28,10 @@ public class SimpleTooltipHolder {
                 Screen screen = Minecraft.getInstance().screen;
                 if (screen != null) {
                     screen.setTooltipForNextRenderPass(this.tooltip, new MenuTooltipPositioner(
-                            new AbstractWidget(
-                                    bounds == null ? 0 : bounds.x(),
+                            new ScreenRectangle(bounds == null ? 0 : bounds.x(),
                                     bounds == null ? 0 : bounds.y(),
                                     bounds == null ? Integer.MAX_VALUE : bounds.w(),
-                                    bounds == null ? Integer.MAX_VALUE : bounds.h(),
-                                    Component.empty()
-                            ) {
-                                @Override
-                                public void renderWidget(@NotNull GuiGraphics guiGraphics, int i, int i1, float v) {}
-
-                                @Override
-                                protected void updateWidgetNarration(@NotNull NarrationElementOutput narrationElementOutput) {}
-                            }), focused);
+                                    bounds == null ? Integer.MAX_VALUE : bounds.h())), focused);
                 }
             }
         }
